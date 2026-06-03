20373970e13d82ce5dd3965ff967fbf3a363fadb
Mushfiqur Rahman Niaz <mushfiqurniaz33@gmail.com>
2026-06-04 03:37:16 +0600

    fix: about us page logo image import for production

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,4 +1,5 @@
 import "../styles/pages/AboutUs.css";
+import {logo} "./src/assets/srothitlogo.png"
 
 function AboutPage() {
   return (
@@ -33,7 +34,7 @@ function AboutPage() {
             <div className="about__img-frame">
               <div className="about__img-placeholder">
                 <img
-                  src="./src/assets/srothitlogo.png"
+                  src={logo}
                   alt="Sroth IT Logo"
                   className="about__img"
                 />
